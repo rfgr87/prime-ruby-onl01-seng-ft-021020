d4523557ce1542fb73c58d844e18800eadab6036
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-12 21:31:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,5 @@ def prime?(number)
   if number < 2
     return false
   else
-    state = (2..number-1).none? {|divisor| number % divisor == 0}
-    return state
+    (2..number-1).none? {|divisor| number % divisor == 0}
 end
